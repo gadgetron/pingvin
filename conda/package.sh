c3490ba17662cb57538f63d6e5cf95105739e55a
Joe Naegele <v-jnaegele@microsoft.com>
2024-11-22 22:48:27 +0000

    Fix CI issues

--- conda/package.sh
+++ conda/package.sh
@@ -11,7 +11,8 @@ Usage: $0
 EOF
 }
 
-output_path="$(dirname "$0")/build_pkg"
+conda_path=$(dirname "$0")
+output_path="${conda_path}/build_pkg"
 
 # Build up channel directives
 channels=(
@@ -25,7 +26,7 @@ channels=(
 
 channel_directives=$(printf -- "-c %s " "${channels[@]}")
 
-python3 validate_versions.py
+python3 "${conda_path}"/validate_versions.py
 
 mkdir -p "$output_path"
-bash -c "conda build --no-anaconda-upload --output-folder $output_path $channel_directives $(dirname "$0")"
+bash -c "conda build --no-anaconda-upload --output-folder $output_path $channel_directives ${conda_path}"
